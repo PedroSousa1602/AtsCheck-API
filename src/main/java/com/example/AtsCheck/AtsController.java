096865204f56c66bbd2285cba542b5fe782d0b14
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-08-24 17:32:30 +0100

    add AtsController with endpoints for ATS check and CV analysis

--- src/main/java/com/example/AtsCheck/AtsController.java
+++ src/main/java/com/example/AtsCheck/AtsController.java
@@ -1,10 +1,29 @@
 package com.example.AtsCheck;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 
 @RestController
 @RequestMapping("/api/ats")
 public class AtsController {
+
+    @GetMapping("/check")
+    public String checkAts() {
+        return "ATS Check API is working!";
+    }
+    @Autowired
+    private ReceivePdf receivePdf;
+
+
+    @PostMapping("/analyze")
+    public ResponseEntity<String> analyzeCv(@RequestParam("file") MultipartFile file) throws IOException {
+
+        String cvText = receivePdf.receivePdf(file.getInputStream());
+        return ResponseEntity.ok(cvText);
+    }
 }
