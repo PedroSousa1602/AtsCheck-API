d33d0925da02fb7d732fba8e1007722e5d1018c2
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-08-25 12:59:38 +0100

    add Opportunity service for analyzing PDF CVs and job descriptions

---
+++ src/main/java/com/example/AtsCheck/Opportunity.java
@@ -0,0 +1,31 @@
+package com.example.AtsCheck;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class Opportunity {
+
+    public String analyzeOpportunity(@NonNull InputStream inputStream, String opText) throws IOException {
+
+        String cvtext = "";
+        try (PDDocument document = Loader.loadPDF(inputStream.readAllBytes())) {
+
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            cvtext = pdfStripper.getText(document);
+
+        } catch (IOException e) {
+            throw new IOException("Error reading PDF CV", e);
+        }
+
+        String jobDescription = opText;
+
+        return cvtext + " " + jobDescription;
+    }
+}
