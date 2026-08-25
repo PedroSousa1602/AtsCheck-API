7177cf49f0750b8e53697288c08c639e058e958b
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-08-25 12:59:48 +0100

    refactor ReceivePdf to improve error message for PDF reading

--- src/main/java/com/example/AtsCheck/ReceivePdf.java
+++ src/main/java/com/example/AtsCheck/ReceivePdf.java
@@ -12,14 +12,14 @@ public class ReceivePdf {
 
     public String receivePdf(@NonNull InputStream inputStream) throws IOException {
 
-        String text = null;
+        String text = "";
         try (PDDocument document = Loader.loadPDF(inputStream.readAllBytes())) {
 
             PDFTextStripper pdfStripper = new PDFTextStripper();
             text = pdfStripper.getText(document);
 
         } catch (IOException e) {
-            throw new IOException("Error reading PDF file", e);
+            throw new IOException("Error reading PDF CV", e);
         }
         return text;
     }
