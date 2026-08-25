6b0f4d73de76b11542cd02bdbf1e3689c7573891
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-08-25 14:46:41 +0100

    add dotenv integration for environment variable loading in AtsCheckApplication

--- src/main/java/com/example/AtsCheck/AtsCheckApplication.java
+++ src/main/java/com/example/AtsCheck/AtsCheckApplication.java
@@ -1,5 +1,6 @@
 package com.example.AtsCheck;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AtsCheckApplication {
 
 	public static void main(String[] args) {
+
+		try {
+			Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+			dotenv.entries().forEach(entry -> {
+				System.setProperty(entry.getKey(), entry.getValue());
+			});
+		} catch (Exception e) {
+
+		}
+
 		SpringApplication.run(AtsCheckApplication.class, args);
 	}
 
