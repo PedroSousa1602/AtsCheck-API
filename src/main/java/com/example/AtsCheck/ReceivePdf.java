3b4529af81e0afbdda41207b305c599593c0f58b
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-08-25 14:44:36 +0100

    refactor ReceivePdf to remove unnecessary null annotation from receivePdf method

--- src/main/java/com/example/AtsCheck/ReceivePdf.java
+++ src/main/java/com/example/AtsCheck/ReceivePdf.java
@@ -2,7 +2,6 @@ package com.example.AtsCheck;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
-import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,7 +9,7 @@ import java.io.InputStream;
 @Service
 public class ReceivePdf {
 
-    public String receivePdf(@NonNull InputStream inputStream) throws IOException {
+    public String receivePdf(InputStream inputStream) throws IOException {
 
         String text = "";
         try (PDDocument document = Loader.loadPDF(inputStream.readAllBytes())) {
