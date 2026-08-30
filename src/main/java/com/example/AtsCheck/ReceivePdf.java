8aefb7858d096f6581071ae17c134e307013a1d4
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-08-30 23:14:05 +0100

    improve error handling in ReceivePdf by using ResponseStatusException for better HTTP response management

--- src/main/java/com/example/AtsCheck/ReceivePdf.java
+++ src/main/java/com/example/AtsCheck/ReceivePdf.java
@@ -3,13 +3,12 @@ import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.tika.Tika;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.server.ResponseStatusException;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
 
 @Service
 public class ReceivePdf {
@@ -23,26 +22,29 @@ public class ReceivePdf {
         Tika tika = new Tika();
         mimeType = tika.detect(bufferedInput);
         if(!mimeType.equals("application/pdf")) {
-            throw new IOException("File is not a PDF. Detected MIME type: " + mimeType);
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "File is not a PDF."
+            );
         }else {
             isPdf = true;
         }
+
         String text = "";
         if(isPdf){
             try (PDDocument document = Loader.loadPDF(bufferedInput.readAllBytes())) {
-
                 if (document.isEncrypted()) {
-                    throw new IOException("PDF CV is encrypted or empty and cannot be read");
+                    throw new ResponseStatusException(
+                            HttpStatus.BAD_REQUEST, "PDF CV is encrypted or empty and cannot be read"
+                    );
                 }
                 PDFTextStripper pdfStripper = new PDFTextStripper();
                 text = pdfStripper.getText(document);
-
             } catch (IOException e) {
-                throw new IOException("Error reading PDF CV", e);
+                throw new ResponseStatusException(
+                        HttpStatus.INTERNAL_SERVER_ERROR, "Error reading PDF CV", e
+                );
             }
         }
         return text;
-
-
     }
 }
