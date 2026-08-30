91af5a0e0387c1ed0f02dcd7b1a2ea59377d0131
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-08-30 23:13:56 +0100

    add PDF validation and improve error handling in analyzeOpportunity method

--- src/main/java/com/example/AtsCheck/Opportunity.java
+++ src/main/java/com/example/AtsCheck/Opportunity.java
@@ -3,8 +3,12 @@ package com.example.AtsCheck;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.tika.Tika;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -13,18 +17,41 @@ public class Opportunity {
 
     public String analyzeOpportunity(InputStream inputStream, String opText) throws IOException {
 
+        boolean isPdf = false;
+        String mimeType = "";
+        InputStream bufferedInput = new BufferedInputStream(inputStream);
+
+        Tika tika = new Tika();
+        mimeType = tika.detect(bufferedInput);
+        if(!mimeType.equals("application/pdf")) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "File is not a PDF."
+            );
+        }else {
+            isPdf = true;
+        }
+
+        if(opText == null || opText.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Opportunity text is missing"
+            );
+        }
+
         String cvtext = "";
-        try (PDDocument document = Loader.loadPDF(inputStream.readAllBytes())) {
-
-            if (document.isEncrypted()) {
-                throw new IOException("PDF CV is encrypted and cannot be read");
+        if(isPdf) {
+            try (PDDocument document = Loader.loadPDF(bufferedInput.readAllBytes())) {
+                if (document.isEncrypted() ) {
+                    throw new ResponseStatusException(
+                            HttpStatus.BAD_REQUEST, "PDF CV is encrypted or empty and cannot be read"
+                    );
+                }
+                PDFTextStripper pdfStripper = new PDFTextStripper();
+                cvtext = pdfStripper.getText(document);
+            } catch (IOException e) {
+                throw new ResponseStatusException(
+                        HttpStatus.INTERNAL_SERVER_ERROR, "Error reading PDF CV", e
+                );
             }
-
-            PDFTextStripper pdfStripper = new PDFTextStripper();
-            cvtext = pdfStripper.getText(document);
-
-        } catch (IOException e) {
-            throw new IOException("Error reading PDF CV", e);
         }
         return cvtext + " " + opText;
     }
